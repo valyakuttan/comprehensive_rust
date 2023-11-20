fdbd1a6f5d073197b7bef137cf92def21f2d5719
valyakuttan <valyakuttan@users.noreply.github.com>
2023-11-20 17:09:33 +0000

    Add some sections

---
+++ src/modules/module_1/lesson_6_basic_syntax/sec_6_4_slices/mod.rs
@@ -0,0 +1,9 @@
+
+mod introduction;
+mod sec_4_1_string;
+
+#[allow(dead_code)]
+pub fn main() {
+    //introduction::main();
+    sec_4_1_string::main();
+}
---
+++ src/modules/module_1/lesson_6_basic_syntax/sec_6_5_functions/sec_5_3_function_overloading.rs
@@ -0,0 +1,30 @@
+/// # Function Overloading
+/// 
+/// Overloading is not supported:
+/// 
+/// Each function has a single implementation:
+/// 
+/// - Always takes a fixed number of parameters.
+/// 
+/// - Always takes a single set of parameter types.
+/// 
+/// Default values are not supported:
+/// 
+/// - All call sites have the same number of arguments.
+/// 
+/// - Macros are sometimes used as an alternative.
+/// 
+/// However, function parameters can be generic:
+///
+/// - When using generics, the standard library’s `Into<T>` can
+///   provide a kind of limited polymorphism on argument types.
+/// 
+
+fn pick_one<T>(a: T, b: T) -> T {
+    if std::process::id() % 2 == 0 { a } else { b }
+}
+
+pub fn main() {
+    println!("coin toss: {}", pick_one("heads", "tails"));
+    println!("cash prize: {}", pick_one(500, 1000));
+}
