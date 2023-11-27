94677a66794557afd15f69a8b9cea27fd935b179
valyakuttan <valyakuttan@users.noreply.github.com>
2023-11-27 12:35:52 +0000

    Add lesson 3 in module 7

---
+++ src/modules/module_7/lesson_3_an_ok_stack/mod.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+
+mod sec_3_1_option;
+mod sec_3_2_generic;
+mod sec_3_3_peek;
+mod sec_3_4_intoiter;
+mod sec_3_5_iter;
+mod sec_3_6_itermut;
+mod sec_3_7_final_code;
+
+
+pub fn main() {
+    //sec_3_1_option::main()
+    //sec_3_7_final_code::main()
+}
--- src/modules/module_7/mod.rs
+++ src/modules/module_7/mod.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 
 mod lesson_2_a_bad_stack;
+mod lesson_3_an_ok_stack;
 
 pub fn main() {
-    lesson_2_a_bad_stack::main();
+    //lesson_2_a_bad_stack::main();
+    lesson_3_an_ok_stack::main()
 }
